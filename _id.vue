b6035c1882e5679ed94c47babde9cf4ddcbef395
okahidenobu <okahidenobu@gmail.com>
2020-01-12 12:54:16 +0900

    Add files via upload

---
+++ _id.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{ fields.title }}</p>
+    <!-- <pre>
+      {{ this.$route.params.id }}
+    </pre> -->
+  </div>
+</template>
+
+<script>
+import { fetchEntries, fetchEntry, fetchPostsByTagId } from "@/api";
+
+export default {
+  async asyncData() {
+    let ret = await fetchEntry(this.$route.params.id);
+
+    console.log(ret);
+
+    return ret;
+  }
+};
+</script>
---
+++ index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- <ul>
+      <li v-for="item in items" :key="item.sys.id">{{ item.fields.title }}</li>
+    </ul> -->
+
+    <!-- fetchEntriesとfetchPostsByTagIdの確認用 -->
+    <!-- <pre>
+      {{ items }}
+    </pre> -->
+
+    <!-- fetchEntryの確認用 -->
+    <pre>
+      {{ fields }}
+    </pre>
+  </div>
+</template>
+
+<script>
+import { fetchEntries, fetchEntry, fetchPostsByTagId } from "@/api";
+
+export default {
+  async asyncData() {
+    // let ret = await fetchEntries("blogPost");
+    let ret = await fetchEntry("6BSvwCHUSVUyyMohbJq2Wb");
+    // let ret = await fetchPostsByTagId("blogPost", "javascript");
+
+    console.log(ret);
+
+    return ret;
+  }
+};
+</script>
